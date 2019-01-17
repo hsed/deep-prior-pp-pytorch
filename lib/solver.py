5c0e1ad81903589284109009090c95830477b765
Haaris <hsed@users.noreply.github.com>
2019-01-17 18:23:26 +0000

    new dropout at testing + averaged

--- lib/solver.py
+++ lib/solver.py
@@ -20,7 +20,8 @@ def train_epoch(model, criterion, optimizer, train_loader, device=torch.device('
         #quit()
 
         train_loss += loss.item()
-        progress_bar(batch_idx, len(train_loader), 'Loss: {0:.4e}'.format(train_loss/(batch_idx+1)))    #
+        progress_bar(batch_idx, len(train_loader),
+                     'Loss: {0:.4e}'.format(train_loss/(batch_idx+1)))    #
         #print('loss: {0: .4e}'.format(train_loss/(batch_idx+1)))
 
 
@@ -30,12 +31,14 @@ def val_epoch(model, criterion, val_loader, device=torch.device('cuda'), dtype=t
 
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(val_loader):
-            inputs, targets = inputs.to(device, dtype), targets.to(device, dtype)
+            inputs, targets = inputs.to(
+                device, dtype), targets.to(device, dtype)
             outputs = model(inputs)
             loss = criterion(outputs, targets)
 
             val_loss += loss.item()
-            progress_bar(batch_idx, len(val_loader), 'Val Loss: {0:.4e}'.format(val_loss/(batch_idx+1)))
+            progress_bar(batch_idx, len(val_loader),
+                         'Val Loss: {0:.4e}'.format(val_loss/(batch_idx+1)))
             #print('loss: {0: .4e}'.format(val_loss/(batch_idx+1)))
 
 
@@ -44,7 +47,7 @@ def test_epoch(model, test_loader, result_collector, device=torch.device('cuda')
 
     with torch.no_grad():
         for batch_idx, (inputs, targets, extra) in enumerate(test_loader):
-            ## test_loader should have a final transformer which returns 
+            ## test_loader should have a final transformer which returns
             ## `y_gt_mm_not_centered` i.e. straight from dataset
             ## `x_cropped_scaled` i.e. no aug but perform crop based on CoM (from dataset) and scale according
             ## to the model requirements i.e 128x128
@@ -55,5 +58,37 @@ def test_epoch(model, test_loader, result_collector, device=torch.device('cuda')
             ## 1, 63  instead of 1, 30
             outputs = model.forward_eval(inputs.to(device, dtype))
 
-            # result college will convert y -> 
+            # result college will convert y ->
             result_collector((inputs, outputs, targets, extra))
+
+
+def test_epoch_dropout(model, test_loader, result_collector, device=torch.device('cuda'), dtype=torch.float, output_rate=10):
+	'''
+		use dropouts to produce output_rate outputs for each input and then use the mean as output
+		we could additionally do the above and supply multiple outcomes.
+
+		important please use batch_size=1 for data_loader!
+		
+		`output_rate` => number of evaluations or outputs per input sample
+	'''
+	## special case here we will be doing dropout and using the mean value ideally...
+	## in this case all dropouts are used, maybe in future we would want to use only one dropout
+	model.train()
+
+	with torch.no_grad():
+		for batch_idx, (inputs, targets, extra) in enumerate(test_loader):
+			assert(targets.shape[0] == 1) # only works currently for batch_size=1
+			output_set = torch.cat([model.forward_eval(inputs.to(device, dtype)) for _ in range(output_rate)])
+			
+			#print("Set shape: ", output_set.shape)
+			
+			## do torch.mean in the right axis
+			output_mean = torch.mean(output_set, dim=0, keepdim=True)
+
+			#print("Mean Shape: ", output_mean.shape)
+			#quit()
+			
+			# result college will convert y -> 
+			result_collector((inputs, output_mean, targets, extra))
+
+			progress_bar(batch_idx*test_loader.batch_size, len(test_loader))
